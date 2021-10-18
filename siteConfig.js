19b8b035ca5275475d8a7715bbac58e3199e7958
Marvie Solis <marvie_solis@ymail.com>
2021-10-18 14:17:41 -0700

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'Marvie Solis',
-  shortName: '',
+  shortName: 'MS',
   description: 'A portfolio consisting of my work and information about me',
   url: 'https://www.jooplaan.com', // Domain of your site without prefix!
   prefix: '/',
